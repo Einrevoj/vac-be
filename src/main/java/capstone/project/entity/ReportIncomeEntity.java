8c17ae215f09a556d1deb539c07fc8b3d1dd7f90
Einrevoj <jovcano28@gmail.com>
2022-10-20 16:10:44 +0800

    added override

--- src/main/java/capstone/project/entity/ReportIncomeEntity.java
+++ src/main/java/capstone/project/entity/ReportIncomeEntity.java
@@ -2,11 +2,13 @@ package capstone.project.entity;
 
 import capstone.project.config.SchemaConfiguration;
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.ZonedDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
@@ -28,4 +30,17 @@ public class ReportIncomeEntity {
     private String proofTrans;
     private ZonedDateTime createdDate;
     private ZonedDateTime modifiedDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ReportIncomeEntity that = (ReportIncomeEntity) o;
+        return incomeId != null && Objects.equals(incomeId, that.incomeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
